c3c6eebb45b3ca88bcc09813d64badb9e0bbb978
Geovanni Roberts <geovanniroberts@Geovannis-MacBook-Pro-2.local>
2022-10-08 13:52:43 -0400

    third push

--- alog.go
+++ alog.go
@@ -39,6 +39,10 @@ func New(w io.Writer) *Alog {
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
+	for {
+		msg := <-al.msgCh
+		go al.write(msg, nil)
+	}
 
 }
 
@@ -50,9 +54,14 @@ func (al Alog) formatMessage(msg string) string {
 }
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
+	al.m.Lock()
+	defer al.m.Unlock()
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
 	if err != nil {
-		al.errorCh <- err
+		go func(err error) {
+			al.errorCh <- err
+		}(err)
+
 	}
 }
 
